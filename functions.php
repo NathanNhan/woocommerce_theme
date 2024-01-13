f55277085ed1abe428d185425f7e9d808cf28d12
Lê Trọng Nhân <44769901+NathanNhan@users.noreply.github.com>
2024-01-13 13:56:45 +0700

    lập trình header và menu

--- functions.php
+++ functions.php
@@ -180,3 +180,20 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+
+/**
+ * Show cart contents / total Ajax
+ */
+add_filter( 'woocommerce_add_to_cart_fragments', 'woocommerce_header_add_to_cart_fragment' );
+
+function woocommerce_header_add_to_cart_fragment( $fragments ) {
+	global $woocommerce;
+
+	ob_start();
+
+	?>
+	<a class="cart-customlocation" href="<?php echo esc_url(wc_get_cart_url()); ?>" title="<?php _e('View your shopping cart', 'woothemes'); ?>"><?php echo sprintf(_n('%d item', '%d items', $woocommerce->cart->cart_contents_count, 'woothemes'), $woocommerce->cart->cart_contents_count);?> – <?php echo $woocommerce->cart->get_cart_total(); ?></a>
+	<?php
+	$fragments['a.cart-customlocation'] = ob_get_clean();
+	return $fragments;
+}
--- header.php
+++ header.php
@@ -29,7 +29,7 @@
       <div class="container">
 		<div class="row">
 			<div class="col-md-5">
-			   <ul class="anoucement__list-left d-flex justify-content-start gap-3 flex-md-column flex-lg-row">
+			   <ul class="anoucement__list-left d-flex justify-lg-content-start justify-content-md-start gap-3 align-items-lg-start flex-md-column flex-lg-row">
 				<!-- Danh sách điện thoại -->
 				<li class="d-flex align-items-center">
 					<i class="bi bi-telephone rounded-circle"></i>
@@ -43,7 +43,7 @@
 			   </ul>
 			</div>
 			<div class="col-md-7">
-			  <ul class="anouncement__list-right d-flex justify-lg-content-end gap-lg-5 justify-content-md-center flex-md-column flex-lg-row gap-md-1 align-items-lg-center">
+			  <ul class="anouncement__list-right d-flex justify-lg-content-end gap-lg-5 justify-content-md-end flex-md-column flex-lg-row gap-md-1 align-items-lg-center">
 				<li class="d-flex align-items-center">
 					<i class="bi bi-truck rounded-circle"></i>
 					Free EU Shipping
@@ -67,34 +67,47 @@
 
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$woocommerce_theme_description = get_bloginfo( 'description', 'display' );
-			if ( $woocommerce_theme_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $woocommerce_theme_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+		<div class="container pt-2 pb-2">
+			<div class="row align-items-center">
+				<!-- cột logo -->
+				<div class="col-md-4 col-12 d-flex justify-content-center justify-content-md-start">
+				 <?php the_custom_logo(); ?>
+				</div>
+				<!-- Cột tìm kiếm -->
+				<div class="col-md-5 col-12">
+	              <?php aws_get_search_form( true ); ?>
+				</div>
+				<!-- Cột giỏ hàng -->
+				<div class="col-md-3 col-12 d-flex justify-content-center justify-content-md-end gap-2">
+					<a href="<?php echo wc_get_cart_url(); ?>"><i  class="bi bi-bag-dash"></i></a>
+					<a class="cart-customlocation text-decoration-none text-black-50" href="<?php echo wc_get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><?php echo sprintf ( _n( '%d item', '%d items', WC()->cart->get_cart_contents_count() ), WC()->cart->get_cart_contents_count() ); ?> – <?php echo WC()->cart->get_cart_total(); ?></a>
+				</div>
+			</div>
+		</div>
+        <!-- thanh menu -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'woocommerce_theme' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
+		<nav id="site-navigation" class="main-navigation bg-primary">
+			<div class="container">
+				<div class="row d-flex justify-content-center align-items-center">
+					<div class="col-12 d-flex justify-content-center align-items-center">
+						
+						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><i class="bi bi-list"></i><?php esc_html_e( 'Primary Menu', 'woocommerce_theme' ); ?></button>
+					</div>
+
+					<div class="col-12 d-md-flex justify-content-md-center text-center">
+						
+									<?php
+										wp_nav_menu(
+											array(
+												'theme_location' => 'menu-1',
+												'menu_id'        => 'primary-menu',
+											)
+										);
+									?>
+					</div>
+				</div>
+			</div>
+			
+			
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
